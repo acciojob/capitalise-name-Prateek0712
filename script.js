2a8706b104034d58efffda518dda31c6e5cb94ad
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-14 15:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const input = document.getElementById('fname');
+
+// Add an event listener to listen for input changes
+input.addEventListener('input', function(event) {
+  // Convert the input value to uppercase and set it back to the input field
+  input.value = input.value.toUpperCase();
+});
